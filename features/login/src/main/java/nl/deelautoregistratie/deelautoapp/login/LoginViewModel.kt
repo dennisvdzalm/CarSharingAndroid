f11e37b9233f7fd9fdcbe790657773f5533eb1e6
Dennis van der Zalm <d.h.vanderzalm@gmail.com>
2019-11-09 17:28:39 +0100

    Linking up the LoginInteractor to LoginViewModel

--- features/login/src/main/java/nl/deelautoregistratie/deelautoapp/login/LoginViewModel.kt
+++ features/login/src/main/java/nl/deelautoregistratie/deelautoapp/login/LoginViewModel.kt
@@ -1,8 +1,33 @@
 package nl.deelautoregistratie.deelautoapp.login
 
 import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import nl.deelautoregistratie.deelautoapp.utils.arch.addTo
+import nl.deelautoregistratie.deelautoapp.utils.arch.schedule
+import nl.deelautoregistratie.deelautoapp.utils.arch.subscribeBy
+import nl.deelautoregistratie.deelautoapp.utils.rx.Scheduler
+import nl.deelautregistratie.domain.interactor.LoginInteractor
+import timber.log.Timber
 import javax.inject.Inject
 
-class LoginViewModel @Inject constructor(): ViewModel(){
+class LoginViewModel @Inject constructor(
+        private val schedulerProvider: Scheduler,
+        private val loginInteractor: LoginInteractor) : ViewModel() {
 
+    private val disposables = CompositeDisposable()
+
+    fun login(username: String, password: String) {
+        loginInteractor.get(LoginInteractor.RequestValues(username, password))
+                .schedule(schedulerProvider)
+                .subscribeBy(
+                        onComplete = { Timber.d("Succesfully logged in") },
+                        error = { Timber.e(it, "Failed to login") }
+                )
+                .addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
 }
--- libraries/core/src/main/java/nl/deelautoregistratie/deelautoapp/utils/arch/Extentions.kt
+++ libraries/core/src/main/java/nl/deelautoregistratie/deelautoapp/utils/arch/Extentions.kt
@@ -1,5 +1,6 @@
 package nl.deelautoregistratie.deelautoapp.utils.arch
 
+import io.reactivex.Completable
 import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.disposables.CompositeDisposable
@@ -16,12 +17,24 @@ fun <T> Single<T>.subscribeBy(success: (T) -> Unit, error: (Throwable) -> Unit):
     }, { throwable -> error.invoke(throwable) })
 }
 
+fun Completable.subscribeBy(onComplete: () -> Unit, error: (Throwable) -> Unit): Disposable {
+    return this.subscribe(
+            { onComplete.invoke() },
+            { throwable -> error.invoke(throwable) }
+    )
+}
+
 fun <T> Flowable<T>.schedule(scheduler: Scheduler): Flowable<T> {
     return this.subscribeOn(scheduler.io())
             .observeOn(scheduler.mainThread())
 }
 
 fun <T> Single<T>.schedule(scheduler: Scheduler): Single<T> {
+    return this.subscribeOn(scheduler.io())
+            .observeOn(scheduler.mainThread())
+}
+
+fun Completable.schedule(scheduler: Scheduler): Completable {
     return this.subscribeOn(scheduler.io())
             .observeOn(scheduler.mainThread())
 }
